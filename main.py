54321ff4bb164c3f476d53151c9fcd4e2c893482
cangencgul <63459021+cangencgul@users.noreply.github.com>
2021-08-12 21:06:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn import preprocessing
+data = np.array([[1, 2], [3, 4]]) 
+
+#return 1 if greater than treshold 0 otherwise
+binaryData= preprocessing.binarize(data, treshold = np.mean(data))
+
+#return the normal distribution of the data
+transformer = preprocessing.StandardScaler().fit(data)
+transformer.transform(data)
+
+#return according to the quantile range ( median = 0)
+transformer = preprocessing.RobustScaler().fit(data)
+transformer.transform(data)
+
+#return (max - value) / (max -min)
+scaler = MinMaxScaler().fit(data)
+scaler.transform(data)
